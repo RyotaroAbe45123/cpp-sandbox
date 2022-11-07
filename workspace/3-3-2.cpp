287371ddb4bfc356aeb0841893408c92e7912322
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-08 00:35:37 +0900

    add: functions

---
+++ workspace/3-3-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int calc_sum_digits(int n) {
+    int sum_digit = 0;
+    while (n > 0) {
+        sum_digit += n % 10;
+        n /= 10;
+    }
+    return sum_digit;
+}
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    int result = 0;
+    for (int i = 1; i <= n; ++i) {
+        int x = calc_sum_digits(i);
+        if (a <= x && x <= b) {
+            result += i;
+        }
+    }
+    cout << result << endl;
+}
