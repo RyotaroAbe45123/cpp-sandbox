4d305e6a5d2345b71a6ffe64fc7adb09f58fceed
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-08 23:48:08 +0900

    add: sort

---
+++ workspace/1/07.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> a(N);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    sort(a.begin(), a.end(), greater<int>());
+
+    int result = 0;
+
+    for (int i = 0; i < N; ++i) {
+        if (i % 2 == 0) {
+            result += a[i];
+        } else {
+            result -= a[i];
+        }
+    }
+
+    cout << result << endl;
+}
