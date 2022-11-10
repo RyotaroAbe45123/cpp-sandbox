120f657b26680c9aece4d2028e4e03460dbed738
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-11 00:32:40 +0900

    add: kagami mochi

--- workspace/1/5-12.cpp
+++ workspace/1/5-12.cpp
@@ -5,4 +5,19 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
+
+    constexpr int M = 101;
+    vector<int> exist(M, 0);
+
+    for (int i = 0; i < N; ++i) {
+        int d;
+        cin >> d;
+        exist[d] = 1;
+    }
+
+    int result = 0;
+    for (auto x : exist) {
+        result += x;
+    }
+    cout << result << endl;
 }
