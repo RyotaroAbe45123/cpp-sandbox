587f187edd4b7f45200f687f50bf90d36c7063d5
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-15 22:42:44 +0900

    update: スプリンクラー

--- workspace/1/5-15.cpp
+++ workspace/1/5-15.cpp
@@ -3,5 +3,40 @@
 using namespace std;
 
 int main() {
-    
+    int N, M, Q;
+    cin >> N >> M >> Q;
+
+    vector<vector<int>> G(N);
+
+    for (int i = 0; i < M; ++i) {
+        int u, v;
+        cin >> u >> v;
+        --u, --v;
+
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+
+    vector<int> col(N);
+    for (auto& coli: col) {
+        cin >> coli;
+    }
+
+    for (int i = 0; i < Q; ++i) {
+        int t, x;
+        cin >> t >> x;
+        --x;
+        cout << col[x] << endl;
+
+        if (t == 1) {
+            for (auto v: G[x]) {
+                col[v] = col[x];
+            }
+        }
+        else {
+            int y;
+            cin >> y;
+            col[x] = y;
+        }
+    }
 }
