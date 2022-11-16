cd1c49906e8e23484ad2f0683baace5d1fbbfdbf
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-16 19:52:57 +0900

    update: 累積和

--- workspace/1/5-16-0.cpp
+++ workspace/1/5-16-0.cpp
@@ -1,6 +1,18 @@
-##
+#include <iostream>
 #include <vector>
+using namespace std;
 
 int main() {
-    vector<int> S(())
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (auto& Ai: A) {
+        cin >> Ai;
+    }
+
+    vector<int> S(N + 1, 0);
+    for (int i = 0; i < N; ++i) {
+        S[i + 1] = S[i] + A[i];
+    }
+    cout << S.back() << endl;
 }
