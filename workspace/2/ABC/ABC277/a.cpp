3d86886de8e45546ef855d1e84b38d3bc0290664
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-28 06:01:25 +0900

    update: ABC277 a

--- workspace/2/ABC/ABC277/a.cpp
+++ workspace/2/ABC/ABC277/a.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
-    
+    int N, X;
+    cin >> N >> X;
+    for (int i = 1; i <= N; ++i) {
+        int x;
+        cin >> x;
+        if (x == X) {
+            cout << i << endl;
+        }
+    }
 }
