8a4875a6aba51559e9a5f92b6e3f3bfb283725c1
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-27 10:16:08 +0900

    update: ABC 278 b

--- workspace/2/ABC/ABC278/b.cpp
+++ workspace/2/ABC/ABC278/b.cpp
@@ -2,13 +2,29 @@
 using namespace std;
 
 bool is_in_24_hours(int h, int m) {
-    return (0 <= h <= 23 && 0 <= m <= 59);
+    if (h < 0) {
+        return false;
+    }
+    else if (h > 23) {
+        return false;
+    }
+    else if (m < 0) {
+        return false;
+    }
+    else if (m > 59) {
+        return false;
+    }
+    else {
+        return true;
+    }
 }
 
 bool misjuded(int h, int m) {
     int a, b, c, d;
-    a, b = h / 10, h % 10;
-    c, d = m / 10, m % 10;
+    a = h / 10;
+    b = h % 10;
+    c = m / 10;
+    d = m % 10;
     int ac, bd;
     ac = a * 10 + c;
     bd = b * 10 + d;
@@ -19,9 +35,10 @@ int main() {
     int H, M;
     cin >> H >> M;
     while (!misjuded(H, M)) {
-        M += 1;
+        ++M;
         if (M == 60) {
-            H, M = H + 1, 0;
+            H = H + 1;
+            M = 0;
         }
         if (H == 24) {
             H = 0;
