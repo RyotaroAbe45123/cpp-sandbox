8b149375339e65dc949b39086a8c5316265d537f
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-27 12:09:35 +0900

    update: ABC 278 c

--- workspace/2/ABC/ABC278/c.cpp
+++ workspace/2/ABC/ABC278/c.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <set>
+#include <utility>
 using namespace std;
 
 int main() {
-    
+    int N, Q;
+    cin >> N >> Q;
+
+    set<pair<unsigned, unsigned>> follows;
+
+    for (unsigned _{}; _ < Q; ++_) {
+        unsigned type, A, B;
+        cin >> type >> A >> B;
+        if (type == 1) {
+            follows.emplace(A, B);
+        }
+        else if (type == 2) {
+            follows.erase({A, B});
+        }
+        else {
+            cout << (follows.count({A, B}) && follows.count({B, A}) ? "Yes": "No") << endl;
+        }
+    }
 }
