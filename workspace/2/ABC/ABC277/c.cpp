db9ab8bd369bb3200335386d45740521be95fda8
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-29 06:50:22 +0900

    update: ABC 277 c

--- workspace/2/ABC/ABC277/c.cpp
+++ workspace/2/ABC/ABC277/c.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <queue>
+#include <map>
+#include <set>
 using namespace std;
 
 int main() {
-    
+    int N;
+    cin >> N;
+    map<int, vector<int>> graph;
+    for (int i = 0; i < N; ++i) {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    queue<int> que;
+    que.push(1);
+    set<int> S;
+    S.insert(1);
+    while (!que.empty()) {
+        int v = que.front();
+        que.pop();
+        for (int i: graph[v]) {
+            if (!S.count(i)) {
+                que.push(i);
+                S.insert(i);
+            }
+        }
+    }
+    cout << *S.rbegin() << endl;
 }
