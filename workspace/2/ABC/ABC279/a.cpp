780acbd808df8f71d6ed1fce363c5da1499144e6
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-30 18:44:31 +0900

    update: ABC279 a

--- workspace/2/ABC/ABC279/a.cpp
+++ workspace/2/ABC/ABC279/a.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    string S;
+    cin >> S;
+    int counter = 0;
+    for (auto& s: S) {
+        if (s == 'v') {
+            counter += 1;
+        }
+        else if (s == 'w') {
+            counter += 2;
+        }
+    }
+    cout << counter << endl;
 }
