7bf997252b9a1cf8e83d1438aa214c9cc5a8159c
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-02 07:46:37 +0900

    update: ABC279 b

--- workspace/2/ABC/ABC279/b.cpp
+++ workspace/2/ABC/ABC279/b.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    string s, t;
+    cin >> s >> t;
+    int sl = s.size(), tl = t.size();
+    for (int i = 0; i <= sl - tl; ++i) {
+        string cur;
+        for (int j = 0; j < tl; ++j) {
+            cur.push_back(s[i+j]);
+        }
+        if (cur == t) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+    return 0;
 }
