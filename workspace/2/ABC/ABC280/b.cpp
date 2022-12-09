f228cdd549423894d0aad1aa2d0caac1ce7f1718
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-10 04:47:42 +0900

    update: ABC280 b

--- workspace/2/ABC/ABC280/b.cpp
+++ workspace/2/ABC/ABC280/b.cpp
@@ -8,7 +8,18 @@ int main() {
     vector<int> s(N);
     for (int i = 0; i < N; ++i) {
         cin >> s[i];
-    }for (int i = 0; i < N; ++i) {
-        cin >> s[i];
     }
+    vector<int> a(N);
+    for (int i = 0; i < N; ++i) {
+        if (i == 0) {
+            a[i] = s[i];
+        }
+        else {
+            a[i] = s[i] - a[i-1];
+        }
+    }
+    for (int i = 0; i < N; ++i) {
+        cout << a[i] << " "
+    }
+    cout << endl;
 }
