251ebc17e819949cf016303dd3e7566f0c9479f7
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-10 15:27:12 +0900

    update: ABC280 b

--- workspace/2/ABC/ABC280/b.cpp
+++ workspace/2/ABC/ABC280/b.cpp
@@ -5,24 +5,21 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    vector<int> s(10);
-    for (int i = 0; i < N; ++i) {
+    vector<int> s(11);
+    s[0] = 0;
+    for (int i = 1; i <= N; ++i) {
         cin >> s[i];
     }
     vector<int> a(10);
     for (int i = 0; i < N; ++i) {
-        int tmp = s[i];
-        for (int j = 0; j < i; ++j) {
-            tmp -= a[j];
-        }
-        a[i] = tmp;
+        a[i] = s[i+1] - s[i];
     }
     for (int i = 0; i < N; ++i) {
-        if (i == 0) {
-            cout << a[i];
+        if (i == N-1) {
+            cout << a[i] << endl;
         }
         else {
-            cout << " " << a[i];
+            cout << a[i] << ' ';
         }
     }
     cout << endl;
