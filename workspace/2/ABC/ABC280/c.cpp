342add7626d9b7527c9184f36584ca6082656ed6
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-10 16:03:31 +0900

    add: ABC280 c

--- workspace/2/ABC/ABC280/c.cpp
+++ workspace/2/ABC/ABC280/c.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    string S, T;
+    cin >> S >> T;
+    for (int i = 0; i < T.size(); ++i) {
+        if (S[i] != T[i]) {
+            cout << i + 1 << endl;
+            break;
+        }
+    }
 }
