d0d3f13d0394924924df2cd616fd8e3d5d27189e
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-11 12:47:42 +0900

    add: ABC281 c

---
+++ workspace/2/ABC/ABC281/c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    long long T;
+    cin >> N >> T;
+    vector<int> A(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i];
+    }
+}
