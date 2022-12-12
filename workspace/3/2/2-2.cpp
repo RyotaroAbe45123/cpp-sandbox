c5c6d1c81981c56f1ebb4ba75c5773f1742c5f95
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-13 08:47:42 +0900

    add: 問題解決2-2

---
+++ workspace/3/2/2-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a % b << endl;
+    cout << abs(a) << endl;
+    cout << pow(a, b) << endl;
+}
