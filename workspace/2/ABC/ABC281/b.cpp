c9e5f3a9109fd9c5c29b5e6ff0642df2216d1a81
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-12 18:49:32 +0900

    update: ABC281 b

--- workspace/2/ABC/ABC281/b.cpp
+++ workspace/2/ABC/ABC281/b.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {}
+bool is_uppercase(const char c) {
+    return 'A' <= c and c <= 'Z';
+}
+
+bool is_digit(const char c) {
+    return '0' <= c and c <= '9';
+}
+
+int main() {
+    string s;
+    cin >> s;
+    bool Yes = true;
+    if (!is_uppercase(s.front())) {
+        Yes = false;
+    }
+    if (!is_uppercase(s.back())) {
+        Yes = false;
+    }
+    if (s.size() != 8) {
+        Yes = false;
+    }
+    for (int i = 1; i < 7; ++i) {
+        if (!is_digit(s[i])) {
+            Yes = false;
+        }
+    }
+    if (s[1] == '0') {
+        Yes = false;
+    }
+    cout << (Yes ? "Yes" : "No") << endl;
+    return 0;
+}
