c9fa6ccfe34580da97d5b2dd2ecdd67f869f8c18
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-14 06:12:25 +0900

    add: ref:3, 2-3

--- workspace/3/2/2-3.cpp
+++ workspace/3/2/2-3.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int cnt = 1000;
+int func1() {
+    return 2020;
+}
+int func2(int pos) {
+    cnt += 1;
+    return cnt + pos;
+}
+
 int main() {
-    
+    int a, b;
+    cin >> a >> b;
+    cout << max(a, b) << endl;
+    cout << min(a, b) << endl;
+
+    cout << func1() << endl;
+    cout << func2(500) << endl;
+    cout << func2(500) << endl;
+    return 0;
 }
