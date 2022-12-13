83348293d7e58c53bb8bb750419eeb42d53f5d1e
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-14 05:33:25 +0900

    update: ref:3, 2-2

--- workspace/3/2/2-2.cpp
+++ workspace/3/2/2-2.cpp
@@ -16,5 +16,7 @@ int main() {
     cout << (a & b) << endl;
     cout << (a | b) << endl;
     cout << (a ^ b) << endl;
+    cout << (a << b) << endl;
+    cout << (a >> b) << endl;
     return 0;
 }
