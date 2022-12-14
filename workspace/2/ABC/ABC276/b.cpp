60f8ee432862b5c4cef2dd217aff8d46ecaa2f14
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-14 21:33:31 +0900

    update: ref:2, ABC276 b

--- workspace/2/ABC/ABC276/b.cpp
+++ workspace/2/ABC/ABC276/b.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    
+    int N, M;
+    cin >> N >> M;
+    vector<vector<int>> S(N);
+    for (int i = 1; i <= M; ++i) {
+        int a, b;
+        cin >> a >> b;
+        S[a-1].push_back(b);
+        S[b-1].push_back(a);
+    }
+    for (int i = 0; i < N; ++i) {
+        sort(begin(S[i]), end(S[i]));
+        cout << S[i].size();
+        for (int s: S[i]) {
+            cout << ' ' << s;
+        }
+        cout << endl;
+    }
+    return 0;
 }
