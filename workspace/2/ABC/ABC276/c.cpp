e95d889353e446b26f176542f6ee0fcd31720cee
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-16 08:44:01 +0900

    add: ABC276 c

--- workspace/2/ABC/ABC276/c.cpp
+++ workspace/2/ABC/ABC276/c.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    
+    int N;
+    cin >> N;
+    vector<int> P(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> P[i];
+    }
 }
