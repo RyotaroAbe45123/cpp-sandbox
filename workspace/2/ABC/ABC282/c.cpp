5c9459f82c45b271b3f4bd98f48f8c70c748f318
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-17 21:39:49 +0900

    update: ref2, ABC282 c

--- workspace/2/ABC/ABC282/c.cpp
+++ workspace/2/ABC/ABC282/c.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    int N;
+    string S;
+    cin >> N >> S;
+    for (int i = 0; i < S.size(); ++i) {
+        cout << S[i] << endl;
+    }
 }
