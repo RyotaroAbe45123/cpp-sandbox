740a9a74745915bb86a09aec21d497f5e725e4c3
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-17 22:05:45 +0900

    add: ref2, ABC282 d

--- workspace/2/ABC/ABC282/d.cpp
+++ workspace/2/ABC/ABC282/d.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    
+    int N, M;
+    cin >> N >> M;
+    vector<vector<int>> G(N);
+    int counter = 0;
+    for (int i = 0; i < M; ++i) {
+        int u, v;
+        cin >> u >> v;
+        G[u-1].push_back(v);
+        G[v-1].push_back(u);
+    }
+    cout << counter << endl;
+    return 0;
 }
