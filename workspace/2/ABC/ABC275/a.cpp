90979bf83e77dcbd79ba94263d38f2f7738ec8f7
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-19 00:12:37 +0900

    update: ref2, ABC275 a

--- workspace/2/ABC/ABC275/a.cpp
+++ workspace/2/ABC/ABC275/a.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    
+    int N;
+    cin >> N;
+    vector<int> H(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> H[i];
+    }
+    int argmax = 0;
+    for (int i = 0; i < N; ++i) {
+        if (H[i] > H[argmax]) {
+            argmax = i;
+        }
+    }
+    cout << argmax + 1 << endl;
 }
