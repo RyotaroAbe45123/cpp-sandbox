ef0cb2107cea212d82f32ec477d8f077c498db2c
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-21 12:49:07 +0900

    update: ref2, ABC274 a

--- workspace/2/ABC/ABC274/a.cpp
+++ workspace/2/ABC/ABC274/a.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
     int a, b;
     cin >> a >> b;
-    double ans = (double)a / (double)b;
-    cout << ans << endl;
+    double ans = double(b) / double(a);
+    cout << fixed << setprecision(3) << ans << endl;
 }
