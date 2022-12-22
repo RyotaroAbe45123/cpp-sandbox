56b7aa4065e8633d12de4dc8f50d8cc84bb42ac4
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-23 00:49:12 +0900

    update: ref2, ABC274 c

--- workspace/2/ABC/ABC274/c.cpp
+++ workspace/2/ABC/ABC274/c.cpp
@@ -9,10 +9,10 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < 2 * n + 1; ++i) {
         int ans = 0;
-        while (c[i] > 0) {
-            c[i] /= 2;
+        while (a[i] > 0) {
+            a[i] /= 2;
             ++ans;
         }
         cout << ans << endl;
