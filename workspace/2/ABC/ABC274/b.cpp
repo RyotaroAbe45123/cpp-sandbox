106de4fb63868a32a5851de31d672e01ddda99dd
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-23 00:37:43 +0900

    update: ref2, ABC274 b

--- workspace/2/ABC/ABC274/b.cpp
+++ workspace/2/ABC/ABC274/b.cpp
@@ -10,4 +10,13 @@ int main() {
     for (int i = 0; i < h; ++i) {
         cin >> c[i];
     }
+    for (int i = 0; i < w; ++i) {
+        int ans = 0;
+        for (int j = 0; j < h; ++j) {
+            if (c[j][i] == '#') {
+                ++ans;
+            }
+        }
+        cout << ans << (i == (w-1) ? '\n' : ' ');
+    }
 }
