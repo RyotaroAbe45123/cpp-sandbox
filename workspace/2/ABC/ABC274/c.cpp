e1471ce30fa41785c156145c5ea3da131637607f
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-23 01:00:09 +0900

    update: ref2, ABC274 c

--- workspace/2/ABC/ABC274/c.cpp
+++ workspace/2/ABC/ABC274/c.cpp
@@ -9,14 +9,15 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
-    cout << 0 << endl;
-    for (int i = 0; i < n; ++i) {
-        int ans = 0;
-        while (a[i] > 0) {
-            a[i] /= 2;
-            ++ans;
-        }
-        cout << ans << endl;
-        cout << ans << endl;
-    }
+    vector<int> b(2 * n + 1, 0);
+    // cout << 0 << endl;
+    // for (int i = 0; i < n; ++i) {
+    //     int ans = 0;
+    //     while (a[i] > 0) {
+    //         a[i] /= 2;
+    //         ++ans;
+    //     }
+    //     cout << ans << endl;
+    //     cout << ans << endl;
+    // }
 }
