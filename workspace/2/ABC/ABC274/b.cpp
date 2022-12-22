a936fb14a659398f244582bb8058ad984ee540c7
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-22 22:09:46 +0900

    update: ref2, ABC274 b

--- workspace/2/ABC/ABC274/b.cpp
+++ workspace/2/ABC/ABC274/b.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     int h, w;
     cin >> h >> w;
-    
+    vector<vector<int>> c(w);
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
+            cin >> c[i][j];
+        }
+    }
 }
