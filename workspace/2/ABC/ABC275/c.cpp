49b6e5d519eb80e22ec238b67b79b51830c9249d
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-24 13:08:44 +0900

    update: ref2, ABC275 c

--- workspace/2/ABC/ABC275/c.cpp
+++ workspace/2/ABC/ABC275/c.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main() {
-    
+    vector<string> S(9);
+    for (int i = 0; i < 9; ++i) {
+        cin >> S[i];
+    }
 }
