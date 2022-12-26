31d6bb9bfc914bf6d2858f46f857cc65969aca7b
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-26 22:49:25 +0900

    update: ref2, ABC272 a

--- workspace/2/ABC/ABC272/a.cpp
+++ workspace/2/ABC/ABC272/a.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        int a;
+        cin >> a;
+        ans += a;
+    }
+    cout << ans << endl;
 }
