a421c245ce62da2e66df8127d5c0306f28ea8b51
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-27 03:38:33 +0900

    update: ref2, ABC271 a

--- workspace/2/ABC/ABC271/a.cpp
+++ workspace/2/ABC/ABC271/a.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    string a = "0123456789ABCDEF";
+    if (n <= 16) {
+        cout << '0' << a[n%16] << endl;
+    }
+    else {
+        cout << a[n/16] << a[n%16] << endl;
+    }
 }
