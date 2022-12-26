f6e7c4047acddd888adf1553e859b76a3200dee0
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-27 04:59:26 +0900

    update: ref2, ABC269 a

--- workspace/2/ABC/ABC269/a.cpp
+++ workspace/2/ABC/ABC269/a.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main() {
-    
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a + b) * (c - d) << endl;
+    cout << "Takahashi" << endl;
+    return 0;
 }
