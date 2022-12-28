1c5144447bd9c73cdb34bb5f666faaefbefc289e
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-28 15:31:26 +0900

    update: ref2, ABC266 a

--- workspace/2/ABC/ABC266/a.cpp
+++ workspace/2/ABC/ABC266/a.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    string s;
+    cin >> s;
+    int l = (s.size() + 1) / 2;
+    cout << s.substr(l-1, 1) << endl;
 }
