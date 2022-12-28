5c989d8a4b176276131678842ec822bedec1e532
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-28 17:06:37 +0900

    update: ref2, ABC264 a

--- workspace/2/ABC/ABC264/a.cpp
+++ workspace/2/ABC/ABC264/a.cpp
@@ -5,6 +5,5 @@ using namespace std;
 int main() {
     int l, r;
     cin >> l >> r;
-    string s = "atcoder";
-    cout << s.substr(l-1, r-l+1) << endl;
+    cout << ((string)"atcoder").substr(l-1, r-l+1) << endl;
 }
