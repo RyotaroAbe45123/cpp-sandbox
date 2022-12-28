777bf4a6031249cb85305406955c22274b11fa1c
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-28 17:00:24 +0900

    update: ref2, ABC265 a

--- workspace/2/ABC/ABC265/a.cpp
+++ workspace/2/ABC/ABC265/a.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main() {
-    
+    int x, y, n;
+    cin >> x >> y >> n;
+    if (x * 3 > y) {
+        cout << ((n/3) * y) + ((n%3) * x) << endl;
+    }
+    else {
+        cout << (n * x) << endl;
+    }
 }
