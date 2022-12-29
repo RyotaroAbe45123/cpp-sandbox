84a5f7e0931917b6aeb9ee115448d2122aae9bc2
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-29 20:20:19 +0900

    update: ref2, ABC263 a

--- workspace/2/ABC/ABC263/a.cpp
+++ workspace/2/ABC/ABC263/a.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+bool cnt(int a, int b, int c, int d, int e) {
+    int ans = 1;
+    if (a == b) ans++;
+    if (a == c) ans++;
+    if (a == d) ans++;
+    if (a == e) ans++;
+    if (ans != 2 && ans != 3) return false;
+    else return true;
+}
 int main() {
-    
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    int ans = 1;
+    if (cnt(a, b, c, d, e) && cnt(b, a, c, d, e) && cnt(c, b, a, d, e) && cnt(d, b, c, a, e) && cnt(e, b, c, d, a)) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
 }
