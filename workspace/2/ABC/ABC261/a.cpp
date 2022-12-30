fe90e2bd39ed03f5f0cf635e72613b37186da99e
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-30 13:02:30 +0900

    update: ref2, ABC261 a

--- workspace/2/ABC/ABC261/a.cpp
+++ workspace/2/ABC/ABC261/a.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 
 int main() {
-    
+    int l1, r1, l2, r2;
+    cin >> l1 >> r1 >> l2 >> r2;
+    if ((r1 - l2 > 0) && (r1 > l2)) cout << r1 - l2 << endl;
+    else cout << 0 << endl;
 }
