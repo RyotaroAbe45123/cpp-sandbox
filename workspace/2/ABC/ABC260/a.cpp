357c4fa3392880d995c24c101e12e3daaa7ee3f8
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-30 13:33:15 +0900

    ad: ref2, ABC260 a

--- workspace/2/ABC/ABC260/a.cpp
+++ workspace/2/ABC/ABC260/a.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    if ((s.at(0) != s.at(1)) || (s.at(1) != s.at(2))) {
+        if (s.at(0) != s.at(1)) cout << s.at(0) << endl;
+        else if ((s.at(1) != s.at(2))) cout << s.at(2) << endl;
+    }
+    else cout << -1 << endl;
 }
