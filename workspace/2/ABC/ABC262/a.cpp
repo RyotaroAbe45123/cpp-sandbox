1252695634ed3063d4de145d06bb11f61071f0c2
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-30 12:55:38 +0900

    update: ref2, ABC262 a

--- workspace/2/ABC/ABC262/a.cpp
+++ workspace/2/ABC/ABC262/a.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-    
+    int y;
+    cin >> y;
+    if (y % 4 == 0) {
+        cout << y + 2 << endl;
+    }
+    else if (y % 4 == 1) {
+        cout << y + 1 << endl;
+    }
+    else if (y % 4 == 2) {
+        cout << y << endl;
+    }
+    else if (y % 4 == 3) {
+        cout << y + 3 << endl;
+    }
 }
