ae03205ffa4e8b04cd5d9aa92631476bb19bf116
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-31 19:47:29 +0900

    update: ref2, ABC258 a

--- workspace/2/ABC/ABC258/a.cpp
+++ workspace/2/ABC/ABC258/a.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    int k;
+    cin >> k;
+    int h = k / 60;
+    int m = k % 60;
+    string hh, mm;
+    if (h == 0) hh = "21";
+    else hh = "22";
+    if (m < 10) mm = "0" + to_string(m);
+    else mm = to_string(m);
+    cout << hh + ":" + mm << endl;
 }
