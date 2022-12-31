d94bd92a8cf902498a4ca848bb90f2d7d0d09b59
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-31 22:24:39 +0900

    update: ref2, ABC254 a

--- workspace/2/ABC/ABC254/a.cpp
+++ workspace/2/ABC/ABC254/a.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    if (n % 100 == 0) cout << "00" << endl;
+    else if (n % 100 < 10) cout << '0' << n % 100 < endl;
+    else cout << n % 100 << endl;
 }
