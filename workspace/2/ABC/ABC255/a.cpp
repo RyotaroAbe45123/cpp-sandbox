cd4abdfe47d245893cf0e75bf4e57b929dd696eb
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-31 21:48:49 +0900

    update: ref2, ABC255 a

--- workspace/2/ABC/ABC255/a.cpp
+++ workspace/2/ABC/ABC255/a.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    
+    int r, c;
+    cin >> r >> c;
+    vector<vector<int>> a(2, vector<int>(2));
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> a.at(i).at(j);
+        }
+    }
+    cout << a[r-1][c-1] << endl;
 }
