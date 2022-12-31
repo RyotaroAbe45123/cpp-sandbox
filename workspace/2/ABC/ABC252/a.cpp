54fc39970e29e5c837de56fd9a3ec0328c433a49
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-01 02:10:36 +0900

    update: ref2, ABC252 a

--- workspace/2/ABC/ABC252/a.cpp
+++ workspace/2/ABC/ABC252/a.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    cout << char('a' + n - 97) << endl;
-    // cout << char(n) << endl;
+    // cout << char('a' + n - 97) << endl;
+    cout << char(n) << endl;
 }
