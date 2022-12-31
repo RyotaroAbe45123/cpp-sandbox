2b700f74cb5fbf02bc1f1d2febb9032d4a8b4186
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-12-31 21:02:09 +0900

    update: ref2, ABC256 a

--- workspace/2/ABC/ABC256/a.cpp
+++ workspace/2/ABC/ABC256/a.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    // long long x = pow(2, n);
+    long long x = 1<<n;
+    cout << x << endl;
 }
