b46e8b3bb7e82c3afdd941f14e0a611bbb438738
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-01 01:46:28 +0900

    update: ref2, ABC253 a

--- workspace/2/ABC/ABC253/a.cpp
+++ workspace/2/ABC/ABC253/a.cpp
@@ -4,10 +4,7 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    if (a <= b && b <= c) {
-        cout << "Yes" << endl;
-    }
-    else if (a >= b && b >= c) {
+    if ((a <= b and b <= c) or (a >= b and b >= c)) {
         cout << "Yes" << endl;
     }
     else {
