9c4371079e9f9734efd9f3899f2850cf188040be
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-02 00:30:48 +0900

    update: ref2, ABC248 a

--- workspace/2/ABC/ABC248/a.cpp
+++ workspace/2/ABC/ABC248/a.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    // cout << s << endl;
+    // return 0;
+    for (int i = 0; i < s.size(); i++) {
+        // cout << s.at(i) << endl;
+        // break;
+        if (s.at(i) != to_string(i)) {
+            cout << s.at(i) << endl;
+            cout << (char) i << endl;
+            break;
+        }
+    }
+    return 0;
 }
