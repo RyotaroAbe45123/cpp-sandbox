749a17a8e41c8f5e7ca68e114eac0200f80d80f0
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-01 15:23:17 +0900

    add: ref2, ABC249 a

--- workspace/2/ABC/ABC249/a.cpp
+++ workspace/2/ABC/ABC249/a.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-    
+    int a, b, c, d, e, f, x;
+    cin >> a >> b >> c >> d >> e >> f >> x;
+    int takahashi = 0, aoki = 0;
+    for (int k = 0; k < x; k++) {
+        if (k % (a + c) < a) takahashi += b;
+        if (k % (d + f) < d) aoki += e;
+    }
+    if (takahashi > aoki) {
+        cout << "Takahashi" << endl;
+    }
+    else if (takahashi == aoki) {
+        cout << "Draw" << endl;
+    }
+    else {
+        cout << "Aoki" << endl;
+    }
+    return 0;
 }
