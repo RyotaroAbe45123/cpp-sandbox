36d4b0ba8dfd4db13c9a42cb2567fbe7f84bfd25
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-01 14:54:09 +0900

    update: ref2, ABC250 a

--- workspace/2/ABC/ABC250/a.cpp
+++ workspace/2/ABC/ABC250/a.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {   
+int main() {
+    int h, w, r, c;
+    cin >> h >> w >> r >> c;
+    int ans = 0;
+    for (int i = 1; i <= h; i++) {
+        for (int j = 1; j <= w; j++) {
+            if ((abs(i - r) + abs(j - c)) == 1) ans++;
+        }
+    }
+    cout << ans << endl;
 }
