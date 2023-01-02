17ca5ad7ccb18730becf9e6d2cba683b45c3b68d
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-02 14:33:38 +0900

    update: ref2, ABC247 a

--- workspace/2/ABC/ABC247/a.cpp
+++ workspace/2/ABC/ABC247/a.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    string s;
+    cin >> s;
+    string t(4, 0);
+    t[0] = '0';
+    t[1] = s[0];
+    t[2] = s[1];
+    t[3] = s[2];
+    cout << t << endl;
 }
