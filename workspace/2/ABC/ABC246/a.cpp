08ce09f047a546ad328b71841815bf6a14469a08
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-02 14:49:55 +0900

    update: ref2, ABC246 a

--- workspace/2/ABC/ABC246/a.cpp
+++ workspace/2/ABC/ABC246/a.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-    
+    int xy[4][4];
+    for (int i = 0; i < 3; i++) {
+        cin >> xy[i][0] >> xy[i][1];
+    }
+    int x, y;
+    if ((xy[0][0] != xy[0][1]) && (xy[0][0] != xy[0][2])) x = xy[0][0];
+    else if ((xy[0][1] != xy[0][0]) && (xy[0][1] != xy[0][2])) x = xy[0][1];
+    else x = xy[0][2];
+    if ((xy[0][0] != xy[1][0]) && (xy[0][0] != xy[2][0])) y = xy[0][0];
+    else if ((xy[1][0] != xy[0][0]) && (xy[1][0] != xy[2][0])) y = xy[1][0];
+    else y = xy[2][0];
+    cout << y << endl;
+    cout << x << " " << y << endl;
 }
