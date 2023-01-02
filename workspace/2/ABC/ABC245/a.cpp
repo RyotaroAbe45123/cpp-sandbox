cef81dca0db5a949b7412e5118e42422ea9ce731
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-02 15:01:12 +0900

    update: ref2, ABC245 a

--- workspace/2/ABC/ABC245/a.cpp
+++ workspace/2/ABC/ABC245/a.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
-    
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a == c) {
+        if (b <= d) {
+            cout << "Takahashi" << endl;
+        }
+        else {
+            cout << "Aoki" << endl;
+        }
+    }
+    else {
+        if (a < c) {
+            cout << "Takahashi" << endl;
+        }
+        else {
+            cout << "Aoki" << endl;
+        }
+
+    }
 }
